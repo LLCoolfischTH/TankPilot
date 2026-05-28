50f51f2bfeda87e39901392df7690b6cef6703fa
Lucas Fischer <ll@mbp-von-lucas.speedport.ip>
2026-05-29 01:57:00 +0200

    cors removed

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const path    = require('path');
 const app  = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/stations',  require('./routes/stations'));

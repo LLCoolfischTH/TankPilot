c028b72b180fb80c1ceed34d3aff87dbd303878b
Lucas Fischer <ll@MacBook-Pro-von-Lucas.local>
2026-06-08 17:01:51 +0200

    Marker grün

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -13,7 +13,7 @@ L.Icon.Default.mergeOptions({
 
 // Blauer Marker für beste Tankstelle
 const bestIcon = new L.Icon({
-  iconUrl:    'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+  iconUrl:    'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
   shadowUrl:  'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
   iconSize:   [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34],
 });
